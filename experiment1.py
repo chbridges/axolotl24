7cf60c73412eb5663cef36a0e5c629c6ba70aa9d
chbridges <47895194+chbridges@users.noreply.github.com>
2024-03-17 01:08:12 +0100

    feat: add plotting over threshold space

--- experiment1.py
+++ experiment1.py
@@ -1,6 +1,12 @@
 import argparse
+import logging
 import os
+import re
 from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+logging.basicConfig(level=logging.INFO)
 
 long = {"fi": "finnish", "ru": "russian"}
 
@@ -11,6 +17,11 @@ parser.add_argument(
 )
 parser.add_argument("--no-pred", action="store_true", help="Only evaluate")
 parser.add_argument("--no-eval", action="store_true", help="Only predict")
+parser.add_argument(
+    "--plot",
+    action="store_true",
+    help="Run experiments for multiple thresholds and plot the results",
+)
 args, positional = parser.parse_known_args()
 
 positional = " ".join(positional)
@@ -25,7 +36,38 @@ pred = pred_dir / f"pred.dev.args.{args.language}.tsv"
 
 pred_dir.mkdir(exist_ok=True)
 
-if not args.no_pred:
-    os.system(f"python {predictor!s} --test {gold!s} --pred {pred!s} {positional}")
-if not args.no_eval:
-    os.system(f"python {scorer!s} --gold {gold!s} --pred {pred!s}")
+
+if not args.plot:
+    if not args.no_pred:
+        os.system(f"python {predictor!s} --test {gold!s} --pred {pred!s} {positional}")
+    if not args.no_eval:
+        os.system(f"python {scorer!s} --gold {gold!s} --pred {pred!s}")
+
+else:
+    plot_dir = Path() / "plots"
+    plot_dir.mkdir(exist_ok=True)
+    handle = f"{args.language}_{args.split}{''.join(positional)}"
+    handle = re.sub(r"(--)|( --)| ", "_", handle)
+
+    thresholds = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    scores = {"ari": [], "f1": []}
+
+    for st in thresholds:
+        logging.info(f"Threshold: {st}")
+        os.system(f"python {predictor!s} --test {gold!s} --pred {pred!s} --st {st} {positional}")
+        os.system(f"python {scorer!s} --gold {gold!s} --pred {pred!s}")
+        with (Path() / "track1_out.txt").open() as file:
+            lines = [line.strip() for line in file.readlines()]
+        scores["ari"].append(float(lines[0][5:]))
+        scores["f1"].append(float(lines[1][4:]))
+
+    plt.figure()
+    plt.plot(thresholds, scores["ari"], label="ARI")
+    plt.plot(thresholds, scores["f1"], label="F1")
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.xlabel("Threshold")
+    plt.title(handle)
+    plt.legend()
+    plt.savefig(plot_dir / handle)
+    logging.info(f"Saved figure to {(plot_dir / handle)!s}")
