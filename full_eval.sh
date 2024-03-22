d4252124d68486729d767e67d3ee3a7ee47756eb
chbridges <47895194+chbridges@users.noreply.github.com>
2024-03-23 00:19:07 +0100

    refactor: final parameter space

--- full_eval.sh
+++ full_eval.sh
@@ -2,12 +2,22 @@
 
 set -e
 
-[ "$#" -ge 3 ] || { echo Usage: "$0 {fi,ru} {dev,train}" >&2; exit 1; }
+[ "$#" -ge 4 ] || { echo Usage: "$0 {fi,ru} {dev,train} model" >&2; exit 1; }
 
-python experiment1.py "$1" "$2" --full
-python experiment1.py "$1" "$2" --full --clusterings 5 
-python experiment1.py "$1" "$2" --full --clusterings 5 --cluster-means
-python experiment1.py "$1" "$2" --full --clusterings 5 --cosine
-python experiment1.py "$1" "$2" --full --clusterings 5 --embed-targets
-python experiment1.py "$1" "$2" --full --clusterings 5 --no-pooling
-python experiment1.py "$1" "$2" --full --clusterings 5 --non-greedy
+python experiment1.py "$1" "$2" --full --model "$3"
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cluster-means
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cluster-means --cosine
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cluster-means --cosine --no-pooling
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cluster-means --cosine --non-greedy
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cluster-means --cosine --no-pooling --non-greedy
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cluster-means --no-pooling
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cluster-means --no-pooling --non-greedy
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cluster-means --non-greedy
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cosine
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cosine --no-pooling
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cosine --non-greedy
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --cosine --no-pooling --non-greedy
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --no-pooling
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --no-pooling --non-greedy
+python experiment1.py "$1" "$2" --full --model "$3" --clusterings 5 --non-greedy
